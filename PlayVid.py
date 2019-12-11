6b2ef787b3c535ecbc591e93b39de813aaad73ec
donnyllionaire <collinstheprogreammer@gmail.com>
2019-12-11 06:22:37 +0300

    using OpenCV library to view and play a video

--- PlayVid.py
+++ PlayVid.py
@@ -3,20 +3,22 @@ import cv2
 
 
 cap= cv2.VideoCapture('vid.mp4')
-print (cap)
+
 
 cv2.destroyAllWindows()
+
+
 while (cap.isOpened()):
-#	ret,frame = cap.read()
-	frame=cap.read()
-	print (frame)
-	break
-	#gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	#c=cv2.imshow('frame',gray)
-	#if cv2.waitKey(25) & 0xFF == ord('q'):
-	#	break
+	ret,frame = cap.read()
+	#frame=cap.read()
+	#print (frame) printing frames in matrix form
+	#break
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	cv2.imshow('frame',gray)
+	if cv2.waitKey(50) & 0xFF == ord('q'):
+		break
 		
-print(frame)
+
 cap.release()
 cv2.destroyAllWindows()
 
