a3436d95bc251f17b97028079ecd3f7bdf6e38bc
donnyllionaire <collinstheprogreammer@gmail.com>
2019-12-11 14:23:02 +0300

    Saving a video using fourcc

--- PlayVid.py
+++ PlayVid.py
@@ -2,10 +2,12 @@ import numpy as np
 import cv2
 
 
-cap= cv2.VideoCapture('vid.mp4')
+cap= cv2.VideoCapture('2.mp4')
 
+#define the codec and create Video Writer object
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+outputObject = cv2.VideoWriter('3t.avi', fourcc, 20.0, (1280,720))
 
-cv2.destroyAllWindows()
 
 
 while (cap.isOpened()):
@@ -15,9 +17,11 @@ while (cap.isOpened()):
 	#break
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	cv2.imshow('frame',gray)
-	if cv2.waitKey(50) & 0xFF == ord('q'):
+	if cv2.waitKey(30) & 0xFF == ord('q'):
 		break
 		
+#release everything after the job is done
+out.release()
 
 cap.release()
 cv2.destroyAllWindows()
